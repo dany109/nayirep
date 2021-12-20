a52dbe435b1883773c7c091dc169eaf63f6002b8
danyal11 <danyal16254@gmail.com>
2021-12-20 17:25:09 +0500

    tasks

--- TASKS.py
+++ TASKS.py
@@ -23,23 +23,3 @@ def encrypt(text,s):
 text = "DEFENDTHEFORT"
 s = 7 #the key to be encrypted 
 
-print ("Plain Text : " + text)
-print ("Shift pattern : " + str(s))
-print ("Cipher: " + encrypt(text,s))
-z = encrypt(text, s)
-def decrypt(text,s):
-   result = ""
-   # transverse the plain text
-   for i in range(len(text)):
-      char = text[i]
-      # Encrypt uppercase characters in plain text
-      
-      if (char.isupper()):
-         result += chr((ord(char) -s -65) % 26 + 65)
-      # Encrypt lowercase characters in plain text
-      else:
-         result += chr((ord(char) - s - 97) % 26 + 97)
-   return result
-#check the above function
-print("DSECRYPTION:",decrypt(z,s))
-print("encryption tasks")
